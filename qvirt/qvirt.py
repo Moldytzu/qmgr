76e4357561896e7916fd6a69659d5f2f1a4c747c
Moldytzu <m.alex123@yahoo.com>
2022-02-26 19:33:52 +0200

    reading json object

--- qvirt/qvirt.py
+++ qvirt/qvirt.py
@@ -16,11 +16,33 @@
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 '''
 
-import sys
+import sys,json
+
+def checkSignature(jsonData: object):
+    passed = False
+    try:
+        passed = jsonData["signature"] == "qvirt" # determine if the signature is qvirt
+    except:
+        passed = False # return false on error
+    return passed
+
+def getJSON(filename: str):
+    file = open(filename,"r") # open file as read-only
+    data = json.loads(file.read()) # read as json data
+    return data # return it
 
 def virtmain(arguments: list[str]):
-    for arg in arguments[1:]: # start handling arguments after the first index (0 or the script's location)
-        print(arg)
+    if(len(arguments) < 2 or len(arguments) > 2):
+        print(f"Usage: python3 {arguments[0]} <path to vm config>") # display usage if there isn't any vm config supplied or if there are more options
+    
+    configFile = arguments[1] # config file path is the first argument
+    jsonData = getJSON(configFile) # get json object
+
+    if(checkSignature(jsonData)):
+        print(jsonData)
+    else:
+        print("Signature check failed!")
+
 
 if __name__ == "__main__":
     virtmain(sys.argv)
